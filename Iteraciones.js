31694543ccdb594db37c1a1a81bee42c5a8cc8a2
Celia Ballesteros <cebalgar@gmail.com>
2021-10-18 16:50:25 +0200

    Iteracion#3

--- Iteraciones.js
+++ Iteraciones.js
@@ -49,3 +49,35 @@ car2["basePrice"]= 70000 + newGlobalBasePrice;
 
 console.log(car1["basePrice"]);
 console.log(car2["basePrice"]);
+
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+
+const x= 10;
+const y= 5;
+alert(x*y);
+
+//1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+const a= 10;
+const b= 2;
+alert(a/b);
+
+//1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+const i= 15;
+const j= 9;
+alert(i % j);
+
+//1.4 Usa el correcto operador de asignación que resultará en x = 15, teniendo dos variables y = 10 y z = 5.
+
+const y= 10;
+const z= 5;
+const x= y+z;
+console.log(x); //la consola muestra 15
+
+//1.5 Usa el correcto operador de asignación que resultará en x = 50,teniendo dos variables y = 10 y z = 5.
+
+const y= 10;
+const z= 5
+const x = y*z;
+console.log(x); // la consola muestra 50.
